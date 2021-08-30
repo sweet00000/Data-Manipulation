f054ca4a3e3a261bbd14368dccabe2bb79991445
sweet00000 <88102370+sweet00000@users.noreply.github.com>
2021-08-30 10:40:40 -0400

    Update merge_csv.R

--- merge_csv.R
+++ merge_csv.R
@@ -7,16 +7,15 @@ for (i in 1:length(files)){
   
   setwd("C:/Users/Sweet/Desktop/TOMATOE/done/MS/fruitscans_21S121")
  
-  
   head1<-basename(files[i])
   
   data <- read.csv(head1 , header=F)
   
   head2 <- data[2,2]
  
-  head <- rbind(head1,head2)
+  head <- cbind(head1,head2)
   
-  newtable <- cbind(newtable,head)
+  newtable <- rbind(newtable,head)
   
   }
 
